ce673dfa0ef6d415334bcb8acdf436b102381b2f
KT-626 <PREFERENCE999@163.COM>
2025-01-22 11:25:33 +0800

    Site updated: 2025-01-22 11:25:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/21/hello-world/","2025/01/21/Git问题：解决port-22-out/"];function toRandomPost(){
+var posts=["2025/01/21/Git问题：解决port-22-out/","2025/01/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
